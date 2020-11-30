8c45386c5fe1d8bb1d5c76905207861ad1154b42
ahsaini <ahsaini@sfu.ca>
2020-11-29 17:13:26 -0800

    first commit

---
+++ BTSQuiz/app/src/main/java/com/sani/btsquiz/StartingScreenActivity.java
@@ -0,0 +1,135 @@
+package com.sani.btsquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+public class StartingScreenActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_QUIZ = 1;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String KEY_HIGHSCORE = "keyHighscore";
+    private TextView textViewHighscore;
+    private int highscore;
+
+    //For spotify background playlist
+    private static final String CLIENT_ID = "88d9312cab154e8c8ea538ebadf458c3";
+    private static final String REDIRECT_URI = "https://com.sani.btsquiz/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_screen);
+
+        textViewHighscore = findViewById(R.id.text_view_highscore);
+        loadHighscore();
+
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+    }
+    public void PlayBackgroundSound(View view) {
+        Intent intent = new Intent(StartingScreenActivity.this, BackgroundSoundService.class);
+        startService(intent);
+    }
+    private void startQuiz() {
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_QUIZ) {
+            if (resultCode == RESULT_OK) {
+                int score = data.getIntExtra(QuizActivity.EXTRA_SCORE, 0);
+                if (score > highscore) {
+                    updateHighscore(score);
+                }
+            }
+        }
+    }
+    private void loadHighscore() {
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        highscore = prefs.getInt(KEY_HIGHSCORE, 0);
+        textViewHighscore.setText("Highscore: " + highscore);
+    }
+    private void updateHighscore(int highscoreNew) {
+        highscore = highscoreNew;
+        textViewHighscore.setText("Highscore: " + highscore);
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, highscore);
+        editor.apply();
+    }
+
+    // FOR SPOTIFY SDK //
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        connected();
+
+                    }
+
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MyActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+//    }
+
+    private void connected() {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play("spotify:playlist:41tr2lr3fSyhIa3uBXBx9X");
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                });
+    }
+
+}
